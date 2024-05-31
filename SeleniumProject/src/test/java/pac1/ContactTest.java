44491ef47fe712db49e0637c1c3fb03afc401d9b
Lenovo <Lenovo@DESKTOP-QJ8EISD>
2024-05-31 17:16:49 +0530

    push

--- SeleniumProject/src/test/java/pac1/ContactTest.java
+++ SeleniumProject/src/test/java/pac1/ContactTest.java
@@ -7,6 +7,16 @@ public class ContactTest {
 	@Test
 	public void createcontactTest()
 	{
+
+		String URL = System.getProperty("Url");
+		String BROWSER = System.getProperty("Browser","Chrome");
+		String USERNAME = System.getProperty("Username");
+		String PASSWORD = System.getProperty("Password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("execute createcontactTest");
 	}
 
--- SeleniumProject/src/test/java/pac2/OrgTest.java
+++ SeleniumProject/src/test/java/pac2/OrgTest.java
@@ -7,6 +7,15 @@ public class OrgTest {
 	@Test
 	public void createOrgTest()
 	{
+		String URL = System.getProperty("Url");
+		String BROWSER = System.getProperty("Browser","Chrome");
+		String USERNAME = System.getProperty("Username");
+		String PASSWORD = System.getProperty("Password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("execute createOrgTest");
 	}
 
